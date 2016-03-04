07d5538221a31730e7ef0cde82bdf4f594818621
Michael <mbotsko7@gmail.com>
2016-03-03 20:05:15 -0800

    database data part 1

--- music.sql
+++ music.sql
@@ -35,4 +35,25 @@ ALTER TABLE album
     FOREIGN KEY (stName)
     REFERENCES studio (stName);
 
+INSERT INTO album
+(title, gpName, stName, dateRecorded, length, numSongs)
+VALUES
+("Shatter Me", "Lindsey Stirling", "Universal Music", 2014-04-29, 00:47:07, 12),
+("Lindsey", "Lindsey Stirling", "Universal Music", 2012-03-29, 01:15:07, 17),
+("Waking Up", "OneRepublic", "Interscope", 2009-11-17, 00:47:28, 11)
+("American Idiot", "Green Day", "Reprise", 2004-09-20, 00:57:12, 13)
+("21st Century Breakdown", "Green Day", "Reprise", 2009-05-15, 00:69:16, 18)
+
+INSERT INTO studio
+VALUES
+("Universal Music", "Universal Avenue", "Monsieur Universal","7145555555"),
+("Interscope", "Something Avenue", "Iris Interscope", "7146666666"),
+("Reprise", "Music Avenue", "Ren Reprise","7147755555")
+
+INSERT INTO recording_group
+VALUES
+("Lindsey Stirling", "Lindsey Stirling", 2008, "Classic/Electric"),
+("OneRepublic", "John Smith", 2005, "Pop"),
+("Green Day", "Adam West", 1990, "Rock")
+
 
